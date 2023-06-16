39bdb403f6e58b2e4a47b3fa421a271032eca2fc
Xenopartyyy <willyawan.m@gmail.com>
2023-06-16 08:43:54 +0700

    [FIX] Perbaikan kecil

--- resources/views/layout/utama.blade.php
+++ resources/views/layout/utama.blade.php
@@ -13,9 +13,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/owlcarousel/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owlcarousel/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href={{ asset('css/boxicon/css/boxicons.min.css') }}>
 
 </head>
 
@@ -29,37 +26,7 @@
   <script src="{{ asset('css/owlcarousel/owl.carousel.min.js') }}"></script>
   <script src="{{ asset('bootstrap/bootstrap.bundle.min.js') }}"></script>  
 
-<script>
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    loop:true,
-    nav:true,
-    margin:10,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },            
-        960:{
-            items:5
-        },
-        1200:{
-            items:6
-        }
-    }
-});
-owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
 
-</script>
 </body>
 
 </html>
