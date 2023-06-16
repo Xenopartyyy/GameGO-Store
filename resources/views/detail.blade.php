c31cc5d21d9e25d2b5727fd44307f88e11a2d4e2
Xenopartyyy <willyawan.m@gmail.com>
2023-06-16 09:47:27 +0700

    [FIX] Perbaikan tampilan halaman

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,33 +1,29 @@
-@extends('layout.utama')
 
-@section('konten')
+    @extends('layout.utama')
     
-
-<div class=" mt-4">
-    <div class="row">
-        <div class="col-3">
-            <div class="produk-avatar">
-               <img src="{{ asset('storage/avatarproduk/' . $produk['avatar']) }}" style="width: 300px">
-            </div>             
-        </div>
-        <div class="col-7">
-            <h3>{{ $produk['nama'] }}</h1>
-            <br><br><br>
-            <p style="text-align: justify">{{ $produk['deskripsi'] }}</p>
-            <div class="bg-info">
-                <h5>Harga</h5>
-                <p class="card-text">{{ 'Rp. ' . number_format($produk['harga'], 0, ',', '.') }}</p>
-            </div>    
-        </div>
-        <div class="col-2">
-            <p>Pesan sekarang di <br><br>
-            <i class='bx bxl-whatsapp' style="color: green; font-size:30px " ></i>085215321818</p>
+    @section('konten')
+        
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 my-4">
+                <div class="produk-avatar text-center">
+                    <img src="{{ asset('storage/avatarproduk/' . $produk['avatar']) }}" class="img-fluid" alt="Produk Avatar">
+                </div>
+            </div>
+            <div class="col-md-8 my-4">
+                <h3>{{ $produk['nama'] }}</h3>
+                <p style="text-align: justify">{{ $produk['deskripsi'] }}</p>
+                <div class="bg-info rounded text-center py-3">
+                    <strong>Harga</strong>
+                    <p class="card-text"><strong>{{ 'Rp. ' . number_format($produk['harga'], 0, ',', '.') }}</strong></p>
+                </div>
+            </div>
+            <div class="col-12 rounded py-3 mb-4" style="background-color: greenyellow">
+                <p class="text-center">Pesan sekarang di<br>
+                <strong><i class='bx bxl-whatsapp' style="color: green; font-size:30px"></i>085215321818</strong></p>
+            </div>
         </div>
     </div>
-</div>
-
-
-
     
-@endsection
-
+    @endsection
+   
